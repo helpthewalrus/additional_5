8b0b700db4cf7331999373ddeeca9c59c898e9a0
helpthewalrus <alinatylets@gmail.com>
2018-09-28 13:01:25 +0300

    task_6_commit_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let symbol of str) {
+    for (let subarray of bracketsConfig) { 
+      if (symbol === subarray[0]) {
+        (subarray[0] === subarray[1] && stack[stack.length - 1] === subarray[0]) ?
+        stack.pop() : stack.push(symbol);
+      } else if (symbol === subarray[1]) {
+        (stack[stack.length - 1] !== subarray[0]) ? 
+        stack.push(symbol) : stack.pop();
+      }  
+    }
+  }
+  return (stack.length === 0 ? true : false);
 }
